5776e910778a01a187018ef23b7954dfdfc62522
Kaaveh Mohamedi <Kaaveh@pm.me>
2020-09-20 22:10:19 +0430

    Update MovieRepository.kt

--- app/src/main/java/ir/kaaveh/recyclerviewmvvm/repository/MovieRepository.kt
+++ app/src/main/java/ir/kaaveh/recyclerviewmvvm/repository/MovieRepository.kt
@@ -12,7 +12,7 @@ suspend fun getMovies(searchMovie: String = "batman"): MoviesResponse? {
     withContext(Dispatchers.IO) {
         response = MyAPI().getMovies(searchMovie)
         if (response.isSuccessful)
-            Log.e("MovieViewModel", "response.isSuccessful is true")
+            Log.v("MovieViewModel", "response.isSuccessful is true")
     }
     return response.body()
 }
