ba89c17be3eda634d326f2d2885496dca94024f3
Kaaveh <kaaveh@pm.me>
2020-10-17 19:37:39 +0330

    fix di bug

--- app/src/main/java/ir/kaaveh/recyclerviewmvvm/view/MainActivity.kt
+++ app/src/main/java/ir/kaaveh/recyclerviewmvvm/view/MainActivity.kt
@@ -2,8 +2,10 @@ package ir.kaaveh.recyclerviewmvvm.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 import ir.kaaveh.recyclerviewmvvm.R
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
